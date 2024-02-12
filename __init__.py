08480fa844e2631b25676a15e7cb6f267b654f90
Squatateur <77248060+Squatateur@users.noreply.github.com>
2024-02-12 11:31:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,7 +37,7 @@ def monhisto():
 
 @app.route("/commits/")
 def moncommit():
-    return render_template("commits.html")
+    return render_template("commit.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
